605c35d8fe6605627400a04757650783779a7f22
CHERISH <1342620995@qq.com>
2023-10-09 18:01:34 +0800

    新增主函数

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
-#include"cherish.h"
+#include "cherish.h"
+#include <stdio.h>
+
+int main() {
+	SeqList a;
+	a.last = 0;
+	insertElem(&a, 1, 1);
+	insertElem(&a, 2, 2);
+	insertElem(&a, 3, 3);
+	insertElem(&a, 4, 4);
+	insertElem(&a, 5, 5);
+	deleteElem(&a, 2);
+	
+	for (int i = 0; i < a.last; i++) {
+		printf("%d ", a.data[i]);
+	}
+	printf("\n3所在的位置为%d个\n", LocateElem(&a, 3));
+	return 0;
+}
